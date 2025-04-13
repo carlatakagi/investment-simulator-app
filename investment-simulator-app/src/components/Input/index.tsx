606ba758036c3fa9588160716d02c545a22f990f
carlatakagi <carlatakagi@gmail.com>
2025-04-13 18:32:41 -0400

    feat: update styles

--- investment-simulator-app/src/components/Input/index.tsx
+++ investment-simulator-app/src/components/Input/index.tsx
@@ -13,7 +13,7 @@ export default function Input({
 }: Props) {
   return (
     <div>
-      <Label style={{ fontSize: 14 }}>{title}</Label>
+      <Label style={{ fontSize: 12 }}>{title}</Label>
       <S.Input
         type={type}
         value={value}
